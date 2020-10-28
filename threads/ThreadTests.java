57557e46a85ec064e45084df6002bf7e320f299f
bys0318 <bys0318@126.com>
2020-10-28 23:26:28 +0800

    modified tests

--- threads/ThreadTests.java
+++ threads/ThreadTests.java
@@ -24,17 +24,17 @@ public class ThreadTests {
 
     public static void joinTest1(){
         // test join
-        System.out.println("JOIN TEST #1: Start");
+        System.out.println("joinTest1: Start!");
         KThread ping0 = new KThread(new PingTest(0));
         ping0.fork();
         ping0.join();
         new PingTest(1).run();
-        System.out.println("JOIN TEST #1: Finished");
+        System.out.println("joinTest1: End!");
     }
         
     public static void joinTest2(){
         // test joining a thread after finished
-        System.out.println("JOIN TEST #2: Start");
+        System.out.println("joinTest2: Start!");
         KThread ping0 = new KThread(new Runnable(){
             public void run() {
                 System.out.println("Thread finished");
@@ -44,12 +44,12 @@ public class ThreadTests {
         KThread.yield();
         ping0.join();
         new PingTest(1).run();
-        System.out.println("JOIN TEST #2: Finished");
+        System.out.println("joinTest2: End!");
     }
 
     public static void conditionTest1(){
 		// test condition2 by cooperation between two threads
-		System.out.println("Condition TEST #1: Start");
+		System.out.println("conditionTest1: Start");
 		final Lock lock = new Lock();
 		final Condition2 condition = new Condition2(lock);
 		KThread thread1 = new KThread(new Runnable(){
@@ -76,7 +76,7 @@ public class ThreadTests {
 		thread1.fork();
 		thread2.fork();
         new PingTest(0).run();
-		System.out.println("Condition TEST #1: End");
+		System.out.println("conditionTest1: End");
     }
 
     private static class PingAlarmTest implements Runnable {
@@ -87,9 +87,9 @@ public class ThreadTests {
         }
 
         public void run(){
-            System.out.println("thread " + which + " waking up in " + ticks + " ticks");
+            System.out.println("Thread " + which + " waking up in " + ticks + " ticks");
             alarm.waitUntil(ticks);
-            System.out.println("thread " + which + " wake up"); 
+            System.out.println("Thread " + which + " wake up"); 
         }
 
         Alarm alarm;
@@ -101,7 +101,7 @@ public class ThreadTests {
         // test three threads on alarm in order
 		Alarm alarm = new Alarm();
 
-        System.out.println("Alarm TEST #1: Start");
+        System.out.println("alarmTest1: Start!");
         KThread thread1 = new KThread(new PingAlarmTest(1, alarm, 3000));
         thread1.fork();
 
@@ -109,69 +109,78 @@ public class ThreadTests {
         thread2.fork();
 
         new PingAlarmTest(0, alarm, 5000).run();
-
-        System.out.println("Alarm TEST #1: End");
+        System.out.println("alarmTest1: End!");
     }
 
     private static class Listener implements Runnable {
+        static int id = 0;
+        // listen for a word
         Listener(Communicator comm){
-            communicator = comm;
-            localID = id++;
+            this.comm = comm;
+            this.myID = id;
+            id++;
         }
         public void run(){
-            System.out.println("L" + localID + ": I'm listening for a word");
-            System.out.println("L" + localID + ": I got the word -> " + communicator.listen());
+            System.out.println("L" + myID + ": I'm listening for a word");
+            System.out.println("L" + myID + ": I got the word -> " + comm.listen());
         }
-        private Communicator communicator;
-        static int id = 0;
-        private int localID;
+        private Communicator comm;
+        private int myID = 0;
     }
 
     private static class Speaker implements Runnable {
+        static int id = 0;
+        // speaks a word
         Speaker(Communicator comm){
-            localID= id++;
-            communicator = comm;
+            this.comm = comm;
+            this.myID = id;
+            id++;
         }
         public void run(){
-            System.out.println("S" + localID + ":I'm Speaking my ID number -> " + localID);
-            communicator.speak(localID);
-            System.out.println("S" + localID + ":I finished Speaking");
+            System.out.println("S" + myID + ":I'm Speaking my ID number -> " + myID);
+            comm.speak(myID);
+            System.out.println("S" + myID + ":I finished Speaking");
         }
-        private Communicator communicator;
-        static int id = 0;
-        private int localID;
+        private Communicator comm;
+        private int myID = 0;
     }
 
     public static void communicatorTest1(){
-		// test 1 listener and 1 speaker, listener first
-		Communicator communicator = new Communicator();
-		new KThread(new Listener(communicator)).fork();
-		new KThread(new Speaker(communicator)).fork();
+        // test 1 listener and 1 speaker, listener first
+        System.out.println("communicatorTest1: Start!");
+		Communicator comm = new Communicator();
+		new KThread(new Listener(comm)).fork();
+		new KThread(new Speaker(comm)).fork();
         new PingTest(0).run();
+        System.out.println("communicatorTest1: End!");
     }
     
 	public static void communicatorTest2(){
-		// test 1 speaker and 1 listener, speaker first
-		Communicator communicator = new Communicator();
-		new KThread(new Speaker(communicator)).fork();
-		new KThread(new Listener(communicator)).fork();
+        // test 1 speaker and 1 listener, speaker first
+        System.out.println("communicatorTest2: Start!");
+		Communicator comm = new Communicator();
+		new KThread(new Speaker(comm)).fork();
+		new KThread(new Listener(comm)).fork();
         new PingTest(0).run();
+        System.out.println("communicatorTest2: End!");
     }
     
 	public static void communicatorTest3(){
-		// test multiple listeners and speakers
-		Communicator communicator = new Communicator();
-		new KThread(new Speaker(communicator)).fork();
-		new KThread(new Listener(communicator)).fork();
-		new KThread(new Listener(communicator)).fork();
-		new KThread(new Speaker(communicator)).fork();
-        new KThread(new Listener(communicator)).fork();
+        // test multiple listeners and speakers
+        System.out.println("communicatorTest3: Start!");
+		Communicator comm = new Communicator();
+		new KThread(new Speaker(comm)).fork();
+		new KThread(new Listener(comm)).fork();
+		new KThread(new Listener(comm)).fork();
+		new KThread(new Speaker(comm)).fork();
+        new KThread(new Listener(comm)).fork();
         new PingTest(0).run();
         new PingTest(1).run();
-        new KThread(new Listener(communicator)).fork();
-        new KThread(new Speaker(communicator)).fork();
-        new KThread(new Speaker(communicator)).fork();
+        new KThread(new Listener(comm)).fork();
+        new KThread(new Speaker(comm)).fork();
+        new KThread(new Speaker(comm)).fork();
         new PingTest(2).run();
         new PingTest(3).run();
+        System.out.println("communicatorTest3: End!");
 	}
 }
